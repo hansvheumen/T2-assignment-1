b3bb28dd040ff35ac755575d98d495e246bfebd4
Heumen,Hans J.B.H.M. van <h.vanheumen@fontys.nl>
2023-02-13 12:34:59 +0100

    control loggin: #define DEBUG true

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
 
-void log(String message, int value = 0)
-{
-  Serial.print(message);
-  Serial.print(":  ");
-  Serial.println(value);
-}
+#define DEBUG true
+
+#if DEBUG == true
+#define log(message, value) Serial.print(message); Serial.print(":  "); Serial.println(value);
+#else
+#define log(message, value) 
+#endif
+
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(A0, INPUT_PULLUP);  
+  log("setup", "done");
 }
 
 void loop()
